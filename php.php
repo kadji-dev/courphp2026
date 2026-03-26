f150cafaf4edea6021ad31d79297a88bee57a8fd
kadji-dev <loickad07@gmail.com>
2026-03-26 10:02:24 +0100

    hello

--- php.php
+++ php.php
@@ -2,3 +2,5 @@
 echo "Hello, World!";
 echo "Welcome to PHP programming.";
 echo "This is a simple PHP script.";
+echo "PHP is a popular server-side scripting language.";
+echo "You can use PHP to create dynamic web pages.";
